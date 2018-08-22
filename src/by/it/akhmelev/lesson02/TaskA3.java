1285b17a5050f8d34b2377b308208b131d0d54b0
Dzmitry Vasin <dzmitry.vasin@gmail.com>
2018-08-22 12:23:34 +0300

    lesson2 A

--- src/by/it/akhmelev/lesson02/TaskA3.java
+++ src/by/it/akhmelev/lesson02/TaskA3.java
@@ -8,7 +8,7 @@ package by.it.akhmelev.lesson02;
 class TaskA3 {
     public static void main(String[] args) {
         int i = 3;
-        int j = 4;
+        int j = 4; 
         int k = i*i + j*j;
         System.out.println(i+"*"+i+"+"+j+"*"+j+"="+k);
     }
